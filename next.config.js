c2adf8d0b04b3a513512bec96cc78ec4720cbe5a
Temigo <temigo@gmx.com>
2021-04-17 14:17:12 -0700

    Connect to backend

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    API_URL: 'https://qbestd-box.herokuapp.com',
+  },
+}
--- pages/query/[id].js
+++ pages/query/[id].js
@@ -2,10 +2,36 @@ import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Link from 'next/link'
 import { Container, Header, Icon, Grid, Form, TextArea, List, Segment, Button, Transition, Step, Table } from 'semantic-ui-react'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+function useQuery (id) {
+  const { data, error } = useSWR(`${process.env.API_URL}/api/v1/results?query_id=${id}`, fetcher)
+
+  return {
+    query: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
+
+function useTest (id) {
+  const { data, error } = useSWR(`${process.env.API_URL}/api/v1/results?test_id=${id}`, fetcher)
+
+  return {
+    test: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
 
 const Query = () => {
     const router = useRouter()
     const { id } = router.query
+    console.log(`${process.env.API_URL}/api/v1/results?query_id=${id}`)
+
+    const { query, isLoading, isError } = useQuery(id)
 
     return (
         <Grid style={{ height: '100vh' }} verticalAlign='middle' centered>
@@ -20,7 +46,13 @@ const Query = () => {
                         Query #{id}
                     </Header>
 
-                    <p>More to come.</p>
+                    <List divided>
+                        {!isLoading && !isError && (query.map((item, itemIndex) => (
+                            <List.Item key={itemIndex}>
+                                {item.test}, {item.match_start}, {item.match_end}, {item.score}
+                            </List.Item>
+                        )))}
+                    </List>
 
                     <Link href='/status/123'>
                         <Button primary>
